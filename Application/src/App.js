6e2081bbeac2916710f11ebf5d51d6136daf45a8
Ryan Sisco <22160702+ryansisco@users.noreply.github.com>
2019-01-29 14:24:55 -0800

    Added Basic UI - NO CSS
    basic UI without css, just html

--- Application/src/App.js
+++ Application/src/App.js
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 // CSS //
 import "./assets/css/styles.css";
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -19,7 +20,20 @@ class App extends Component {
   render() {
     return (
         <div>
-            
+         <h1>DIVA</h1> 
+         <button type="button">Options</button><br/><br/>
+         <button type="button">Input</button>
+         <button type="button">Output</button><br/><br/>
+         <form>
+         X-Axis: <input type="text" name="xaxis1"/>
+         <input type="text" name="xaxis2"/><br/>
+         Y-Axis: <input type="text" name="yaxis1"/>
+         <input type="text" name="yaxis2"/><br/>
+         Z-Axis: <input type="text" name="zaxis1"/>
+         <input type="text" name="zaxis2"/>
+         </form>
+         <button type="button">Render</button>
+         <button type="button">Filter</button>
         </div>
     )
   }
