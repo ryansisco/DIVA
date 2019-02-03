cf06e558ff4459334a370e84daac0a8a9de76281
unknown <ryansisco8@gmail.com>
2019-02-02 17:01:10 -0800

    added button and logo resizing

--- Application/src/App.js
+++ Application/src/App.js
@@ -3,40 +3,64 @@ import React, { Component } from "react";
 // REDUX //
 import { connect } from "react-redux";
 
+import Toggle from "react-toggle-component";
+
 // CSS //
 import "./assets/css/styles.css";
+import "react-toggle-component/styles.css";
 
+// FILES //
 import logo from './assets/img/logo.png';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: false
+    };
   }
 
   componentDidMount(){
     document.title=process.env.TITLE;
   }
 
+  Togglemenu(){
+    if (this.state.checked==1) {
+        return (
+          <div>
+            <br/><br/>
+            <button type="subbutton">Input</button>
+             <button type="subbutton">Output</button><br/><br/>
+             <form>
+             X-Axis: <input type="text" name="xaxis1"/>
+             <input type="text" name="xaxis2"/><br/>
+             Y-Axis: <input type="text" name="yaxis1"/>
+             <input type="text" name="yaxis2"/><br/>
+             Z-Axis: <input type="text" name="zaxis1"/>
+             <input type="text" name="zaxis2"/>
+             </form>
+             <button type="subbutton">Render</button>
+             <button type="subbutton">Filter</button>
+          </div>
+        );
+    }
+    if (this.state.checked==0) {
+      return (
+        <div></div>
+        );
+    }
+  }
+
   render() {
+    const buttonText = this.state.checked ? 'Hide Menu' : 'Show Menu';
     return (
         <div>
-         <img src={logo} width="50%" height="50%"/><br/>
-         <button type="button">Options</button><br/><br/>
-         <button type="button">Input</button>
-         <button type="button">Output</button><br/><br/>
-         <form>
-         X-Axis: <input type="text" name="xaxis1"/>
-         <input type="text" name="xaxis2"/><br/>
-         Y-Axis: <input type="text" name="yaxis1"/>
-         <input type="text" name="yaxis2"/><br/>
-         Z-Axis: <input type="text" name="zaxis1"/>
-         <input type="text" name="zaxis2"/>
-         </form>
-         <button type="button">Render</button>
-         <button type="button">Filter</button>
+         <center><img src={logo} width = "25%" height = 'auto'/></center><br/>
+         <button onClick={() => this.setState({checked:!this.state.checked})}>{buttonText}</button><br/>
+         {this.Togglemenu()}
         </div>
-    )
+    );
   }
 }
 
