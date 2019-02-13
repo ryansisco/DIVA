2445405ec5890a542b612338c841ffaa068ecde3
Matt <jansemat@oregonstate.edu>
2019-02-12 17:07:14 -0800

    Updated App.js with initial JavaScript parsing functionalities

--- Application/src/App.js
+++ Application/src/App.js
@@ -10,9 +10,101 @@ import "./assets/css/styles.css";
 // FILES //
 import logo from './assets/img/logo.png';
 
-const options = [
-  'IOC', 'Date', 'Country', 'IOC Type', 'Sector'
-]
+var options = [];
+//   'IOC', 'Date', 'Country', 'IOC Type', 'Sector'
+// ]
+
+//////////////////////////////////////////////////////////////
+//////// CSV PARSER JAVASCRIPT FUNCTIONS - BEGIN /////////////
+//////////////////////////////////////////////////////////////
+
+const userSelect = [
+'IOC', 'Date', 'Country Code'
+];
+
+//Return the first line of the CSV file as an array
+//INPUT: contents of entire CSV file
+//OUTPUT: div = ['row1_col1', 'row1_col2', ..., 'row1_colN']
+function uiToCSV_object1(content){
+  var str = content;
+  var first = str.split('\n')[0];
+  var div = first.split(',');
+  return div;
+};
+
+//
+function uiToCSV_object2(content){
+  var object = [];
+
+  return object;
+};
+
+function uiToCSV_object3(content){
+  var object = [];
+
+  return object;
+};
+
+//Returns first object for CSV --> 3DV
+//USES GLOBAL: userSelect
+function parserTo3DV_object1(){
+
+  var select1 = userSelect[0], 
+      select2 = userSelect[1],
+      select3 = userSelect[2];
+
+  var type1 = "string", 
+      type2 = "timestamp",
+      type3 = "string";
+
+  var text = '{"xColumn":{"title":"'+select1+'","type":"'+type1+'"},"yColumn":{"title":"'+select2+'","type":"'+type2+'"},"zColumn":{"title":"'+select3+'","type":"'+type3+'"}}';
+  var object = JSON.parse(text);
+
+  return object;
+};
+
+//Returns second object for CSV --> 3DV
+//USES GLOBAL: userSelect
+function parserTo3DV_object2(content){
+  var object = [];
+  var text = '[';
+  var contentArray = content.split('\n');
+  var contentArrayLength = contentArray.length;
+
+  var xTemp, xOffset = contentArray[0].split(',').indexOf(userSelect[0]);
+  var yTemp, yOffset = contentArray[0].split(',').indexOf(userSelect[1]);
+  var zTemp, zOffset = contentArray[0].split(',').indexOf(userSelect[2]);
+  var textTemp = "";
+
+  for (var i = 1; i < contentArrayLength; i++){
+    var subdataTemp = {};
+    var rowArray = contentArray[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+    var rowArrayLength = rowArray.length;
+
+    if(rowArray[0][0] == '"'){
+      //rowArray[0][0] = '_';
+    }
+    xTemp = rowArray[xOffset], yTemp = rowArray[yOffset], zTemp = rowArray[zOffset];
+    textTemp += '{"x":"'+xTemp+'","y":"'+yTemp+'","z":"'+zTemp+'"}';
+
+    //TODO: subdata
+
+    if(i != (contentArrayLength - 1)){
+      textTemp += ",";
+    }
+  }
+  
+  text = text + textTemp + "]";
+  //console.log(text);
+  
+  //object = JSON.parse(text);
+
+  return text;
+};
+
+//////////////////////////////////////////////////////////////
+////////// CSV PARSER JAVASCRIPT FUNCTIONS - END /////////////
+//////////////////////////////////////////////////////////////
 
 class App extends Component {
 
@@ -69,6 +161,15 @@ class App extends Component {
     event.preventDefault();
     var file = event.target.files[0];
     this.setState({file});
+
+    var reader = new FileReader();
+    reader.onload = function(event){
+      var contents = event.target.result;
+      options = uiToCSV_object1(contents);
+      console.log(parserTo3DV_object2(contents));
+    };
+    reader.readAsText(file);
+
     console.log(file.name);
   }
   FileDialogue(){
@@ -91,6 +192,7 @@ class App extends Component {
   Togglemenu(){
     const defaultOption = this.state.selected;
     const placeHolderValue = typeof this.state.selected === 'string' ? this.state.selected : this.state.selected.label;
+
     if (this.state.checked==1) {
         return (
           <Draggable>
