febf61be6c5f8b894a7398a705e76c89ca7d668b
Bhavya Parikh <parikhb@oregonstate.edu>
2019-05-14 18:22:23 -0700

    added slider, ui changes and more feature

--- Application/src/components/GraphicOptions.js
+++ Application/src/components/GraphicOptions.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { SketchPicker } from 'react-color';
+import ReactDom from 'react-dom';
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css'
+import Slider from 'rc-slider';
+import Tooltip from 'rc-slider';
+
+import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
 import { updateGraphData } from '../redux/actions';
 
 let state = {
 	color: {
-		x: null,
+		x: [],
 		y: [],
-		z: [],
-		Background: []
-	}
-	//Camera:{
-			//reset:1
-			//rotate:1
-	//}
+		z: []
+	},
+	thickness:{
+		a:1,
+		b:2,
+		c:3
+	},
+	backgroundColor:
+		 'white'
+	,
+	visualization:
+		'scater'
+	,
+	rotaion:true,
+	axesLabels: true,
+	axesNames:false
+
 }
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+const Handle = Slider.Handle;
+
+const handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+};
+
+const wrapperStyle = { width: 400, margin: 50 };
 
 class GraphicOptions extends Component {
     constructor(props) {
@@ -41,9 +78,7 @@ class GraphicOptions extends Component {
                 newState[axis] = !newState[axis];
             }
         })
-        //this.setState(newState);
-				this.setState({colorX:!this.state.colorX})
-				this.setState({colorX:!this.state.colorY})
+        this.setState(newState);
 	}
 
 	renderColorX(){
@@ -105,7 +140,7 @@ class GraphicOptions extends Component {
 				);
 		}
 	}
-
+//this.setState({ visualization: 'cloud' })
 
     render() {
 				const graphData = this.props.graphData.data;
@@ -113,22 +148,26 @@ class GraphicOptions extends Component {
             <div>
 
 						<div className="graphicOptionsBox">Type of Visualization<br/>
-						<input type="radio" value="Ascending" name="order" onChange= {this.state.xOrder === "Ascending"}/> Blob data
-						<input type="radio" value="Descending" name="order" onChange={this.state.xOrder === "Descending"}/> data
-						<br/>Camera<br/>
-						<input type="checkbox" value="Rotate" name="order"/> Camera Reset
+						<input type="radio" value="Cloud" name="order" onChange={() => this.setState( "visualization: cloud")} checked= {this.state.visualization === "cloud"}/> Cloud
+						<input type="radio" value="Scater" name="order"onChange={() => this.setState( "visualization: scatter")} checked={this.state.visualization === "Scater"}/> Scater
+						<br/><br/>Camera<br/>
+						<button className = "button" onClick = {() => {this.setState()}}> Camera Reset </button>
 						<input type="checkbox" value="Rotate" name="order"/> Enable Rotate
-						<br/>Color Options<br/>
-						<button className = "button" onClick = {() => {this.setState()}}> Background Color</button>
-						{this.renderColorBackground()}
-					  <button className="button"onClick = {() => this.toggleColorMenu('x')}>{graphData.xColumn ? graphData.xColumn.name : 'X'}</button>
+						<br/><br/>Background Color<br/>
+						<input type="radio" value="Black" name="order" onChange={() => this.setState( "backgroundColor: black")} checked= {this.state.backgroundColor === "Black"}/> Black
+						<input type="radio" value="White" name="order" onChange={() => this.setState( "backgroundColor: white")}checked={this.state.backgroundColor === "White"}/> White
+						<br/><br/>Color Options<br/>
+						<button className="button"onClick = {() => this.toggleColorMenu('x')}>{graphData.xColumn ? graphData.xColumn.name : 'X'}</button>
 						<button className="button"onClick = {() => this.toggleColorMenu('y')}>{graphData.yColumn ? graphData.yColumn.name : 'Y'}</button>
 						<button className="button"onClick = {() => this.toggleColorMenu('z')}>{graphData.zColumn ? graphData.zColumn.name : 'Z'}</button>
 						{this.renderColorX()}
 						{this.renderColorY()}
 						{this.renderColorZ()}
-						<br/>Thickness<br/>
-						
+						<br/><br/>Thickness<br/>
+						<div style={wrapperStyle}>
+							<p>Slider with fixed values</p>
+							<Slider min={20} defaultValue={20} marks={{ 20: 20, 40: 40, 100: 100 }} step={null} />
+							</div>
 						<br/><br/>
 						<button className = "button" onClick = {() => {this.setState()}}> Save Options </button>
 						</div>
